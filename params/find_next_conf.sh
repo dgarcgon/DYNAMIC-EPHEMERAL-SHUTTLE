25a80e0a49a194db4fb5b3c39aaeec357ea50bd7
John Ruiz-Atos <91595254+p24johnruiz@users.noreply.github.com>
2025-03-31 17:25:21 +0200

    Update find_next_conf.sh

--- params/find_next_conf.sh
+++ params/find_next_conf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-DIR="/home/dgarc/proj_code_files"
+DIR="."
 string_a_buscar="VAR_TEMPLATE_PROJECT_CODE_LOWERCASE"
  
 FS='
@@ -9,7 +9,7 @@ for item in $(ls $DIR)
 do
         encontrado=$(grep $string_a_buscar $DIR/$item | cut -b 1-7)
         if [[ $encontrado == 'PROJECT' ]]; then
-        sed -i 's/'$string_a_buscar'/'$1'/g' /home/dgarc/proj_code_files/$item
+        sed -i 's/'$string_a_buscar'/'$1'/g' $item
         mv $DIR/$item $DIR/$1_params.env
         echo $item
         exit
